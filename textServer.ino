8552622bed67d2fa4cb0eb558b43278aa6b3d9c2
Mohd Safwan <kdbeatbox@gmail.com>
2020-04-24 22:10:22 +0530

    Add files via upload

---
+++ textServer.ino
@@ -0,0 +1,142 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-input-data-html-form/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
+
+AsyncWebServer server(80);
+
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "ESP32";
+const char* password = "12345678";
+
+const char* PARAM_STRING = "inputString";
+
+// HTML web page to handle 3 input fields (inputString, inputInt, inputFloat)
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script>
+    function submitMessage() {
+      setTimeout(function(){ document.location.reload(false); }, 500);   
+    }
+  </script></head><body>
+  <div align='center'>
+  <style>html{width:100%; height:100%;}body{width:100%; height:100%; background-color:#DDD;}</style>
+  <p>
+    <h1>Electronic Design Lab</h1>
+    <h2>Group D-13</h2>
+  </p>
+  <form action="/get" target="hidden-form">
+    Current text: %inputString%
+    <br>
+    Enter text to display
+    <br>
+    <input type="text" name="inputString">
+    <br>
+    <input type="submit" value="Display" onclick="submitMessage()">
+  </form>
+  </div>
+  <iframe style="display:none" name="hidden-form"></iframe>
+</body></html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+String readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\r\n", path);
+  File file = fs.open(path, "r");
+  if(!file || file.isDirectory()){
+    Serial.println("- empty file or failed to open file");
+    return String();
+  }
+  String fileContent;
+  while(file.available()){
+    fileContent+=String((char)file.read());
+  }
+  Serial.println(fileContent);
+  return fileContent;
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\r\n", path);
+  File file = fs.open(path, "w");
+  if(!file){
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("- file written");
+  } else {
+    Serial.println("- write failed");
+  }
+}
+
+// Replaces placeholder with stored values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "inputString"){
+    return readFile(SPIFFS, "/inputString.txt");
+  }
+  return String();
+}
+
+void setup() {
+  Serial.begin(115200);
+  // Initialize SPIFFS
+    if(!SPIFFS.begin(true)){
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
+    }
+    
+//  WiFi.mode(WIFI_STA);
+  WiFi.softAP(ssid, password);
+  delay(2000);
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.softAPIP());
+
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/get?inputString=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    // GET inputString value on <ESP_IP>/get?inputString=<inputMessage>
+    if (request->hasParam(PARAM_STRING)) {
+      inputMessage = request->getParam(PARAM_STRING)->value();
+      writeFile(SPIFFS, "/inputString.txt", inputMessage.c_str());
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/text", inputMessage);
+  });
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void loop() {
+  // To access your stored values on inputString, inputInt, inputFloat
+  String yourInputString = readFile(SPIFFS, "/inputString.txt");
+  Serial.print("*** Your inputString: ");
+  Serial.println(yourInputString);
+  delay(5000);
+}
